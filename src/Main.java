5cb124c117cc49cb682b93bb4de681f639a1f288
Miroslav Tsadkin <Luckymirik@mail.ru>
2024-04-03 16:59:17 +0300

    Finish

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
-public class Main {
-    public static void main(String[] args) {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class Main {
+    static JPanel panel = new JPanel();
+    static void addObject(JComponent component){
+        component.setAlignmentX(0.5F);
+        panel.add(component);
+        panel.add(Box.createVerticalStrut(10));
+    }
+    public static void main(String[] args) {
+        panel.setLayout(new BoxLayout(panel,BoxLayout.Y_AXIS));
+        panel.add(Box.createVerticalStrut(10));
+        JFrame frame = new JFrame("JTextArea");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,300);
+        frame.setLocationRelativeTo(null);
+
+        JTextField jTextField = new JTextField();
+        jTextField.setMaximumSize(new Dimension(300,30));
+        addObject(jTextField);
+
+        JButton jButton = new JButton("Записать");
+        addObject(jButton);
+
+        JTextArea jTextArea = new JTextArea();
+        jTextArea.setRows(4);
+        JScrollPane scrollPane = new JScrollPane(jTextArea,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setMaximumSize(new Dimension(300,100));
+        addObject(scrollPane);
+
+        jButton.addActionListener(e -> jTextArea.setText(jTextArea.getText()+(jTextArea.getText().length()==0?"":"\n")
+                +jTextField.getText()));
+
+        frame.add(panel);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
